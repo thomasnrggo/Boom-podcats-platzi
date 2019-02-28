067ba2042f83cd5e882a4d2b6a6805760b009844
thomasnrggo <thomasnrggo@gmail.com>
2019-02-27 23:12:52 -0600

    pantalla acerca de

--- pages/about.js
+++ pages/about.js
@@ -8,32 +8,61 @@ export default class extends React.Component {
             <Layout title="Acerca de..." footer={false}>
                 
                 <div className="container">
-                    <h3>Hecho por 
-                        <br /> 
-                        <Link href="https://www.instagram.com/thomas_nrg/">
-                            <a>@thomas_nrg</a>
+
+                    <img className="logo" src="/static/platzi-logo.png" alt="platzi" /> 
+                    <p>
+                        <b>¡Boom podcats!</b> nace del proyecto realizado bajo la guía del profesor 
+                        <Link href="https://twitter.com/robertcode"><a> @robertcode </a></Link> ‏
+                        en el curso de next de platzi. La aplicación funciona gracias al api de 
+                        <Link href="https://github.com/audioBoom/api"><a> audioBoom </a></Link> 
+                        y esta hecha desde 0 con nextJs, puedes ver el codigo de la aplicacion 
+                        <Link href="https://github.com/thomasnrggo/Boom-podcats-platzi"><a> aquí.</a></Link>
+                    </p>
+                    <div className="volver">
+                        <Link href="/">
+                            <a>Volver a Inicio</a>
                         </Link>
-                    </h3>
-                    <p>Proyecto Realizado para el curso de NextJs de Platzi.</p>
-                    <img className="logo" src="/static/platzi-logo.png" alt="platzi" />
+                    </div>
+                      
                 </div>
+
+                <h3>Hecho por<Link href="https://twitter.com/thomas_nrg"><a> @thomas_nrg </a></Link></h3>
 
                 <style jsx>{`
                     .container {
                         padding: 100px 30px;
                     }
                     h3 {
+                        width: 100%;
+                        position: fixed;
+                        bottom: 2.5%;
+                        margin: 0 auto;
+                        font-size: .9em;
                         color: white;
                         text-align: center;
                     }
                     p {
                         color: white;
                         text-align: center;
+                        line-height: 1.3em;
                     }
                     .logo {
-                        max-width:10%;
+                        max-width:20%;
                         display: block;
                         margin: 0 auto;
+                    }
+                    a {
+                        color: #fff;
+                        text-decoration: none;
+                        transition: ease .4s;
+                        font-weight: bold;
+                    }
+                    a:hover {
+                        color: #6fc629;
+                    }
+                    .volver {
+                        padding: 50px 12px;
+                        text-align: center;
                     }
                 `}</style>
 
